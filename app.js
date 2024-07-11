65bbf99fa7cd483cd1f5b1fcb994ecee0d71a46a
praneetha <praneetha1818@gmail.com>
2024-07-11 09:24:49 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,6 @@
+$("#card").wScratchPad({
+  size: 100, // The size of the brush/scratch.
+  bg: `Images/img1.png`, // Background (image path or hex color).
+  fg: `Images/front.png`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
