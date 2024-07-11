0273c64b57b9b0f09dc8575e1d09a9ecaea0782a
praneetha <praneetha1818@gmail.com>
2024-07-11 10:45:30 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+var num = Math.floor(Math.random() * 5) + 1;
+
+
 $("#card").wScratchPad({
   size: 100, // The size of the brush/scratch.
-  bg: `Images/img1.png`, // Background (image path or hex color).
+  bg: `Images/img ${num}.png`, // Background (image path or hex color).
   fg: `Images/front.png`, // Foreground (image path or hex color).
   cursor: "pointer", // Set cursor.
 });
